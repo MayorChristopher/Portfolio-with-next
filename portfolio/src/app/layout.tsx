0452a29b19ce9b11616742d6be4f13b6a6086089
Mayor Ugochukwu <mayoru24@gmail.com>
2024-03-02 19:58:10 +0000

    WIP

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -1,16 +1,27 @@
+import Header  from '@/components/header';
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
 export const metadata = {
-  title: 'My Portfolio | Mayor Ugochukwu',
-  description: 'Web Developer with 4 years experience',
-}
+  title: "My Portfolio | Mayor Ugochukwu",
+  description: "Mayor is a Web Developer with 3 years of experience",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+        <div className=" bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] -z-10 h-[31.25rem] w-[32.25rem] rounded-full blur-[10rem] sm: w-[68.75rem]"></div>
+        <div className=" bg-[#dbd7fb] absolute top-[-1rem] left-[35rem] -z-10 h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm: w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <Header />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,10 +1,3 @@
 export default function Home(){
-    return (
-        <>
-        <h1>My Profile</h1>
-        <li>Know more</li>
-        <li>Experience</li>
-        <li>My Work</li>
-        </>
-    );
+    return 
 }
